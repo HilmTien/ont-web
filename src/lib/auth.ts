7da3e5ec1a61e46d664cfbd11996e7bc9f49eee7
HilmTien <tient2477@gmail.com>
2025-02-21 22:06:27 +0100

    feat: add necessary boilerplate

---
+++ src/lib/auth.ts
@@ -0,0 +1,14 @@
+import { SupabaseAdapter } from "@auth/supabase-adapter";
+import NextAuth from "next-auth";
+import Osu from "next-auth/providers/osu";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [Osu],
+  adapter: SupabaseAdapter({
+    url: process.env.SUPABASE_URL!,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY!,
+  }),
+  session: {
+    strategy: "database",
+  },
+});
