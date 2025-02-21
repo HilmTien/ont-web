794cf5b6ea4d3e8535f568abf56007a4a58964e8
HilmTien <tient2477@gmail.com>
2025-02-21 22:12:59 +0100

    feat: add required supabasejs env variables

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,7 +5,7 @@ import Osu from "next-auth/providers/osu";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Osu],
   adapter: SupabaseAdapter({
-    url: process.env.SUPABASE_URL!,
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
     secret: process.env.SUPABASE_SERVICE_ROLE_KEY!,
   }),
   session: {
