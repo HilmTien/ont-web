6717f2ca47d4a6717323c56c574d295c882d130d
HilmTien <tient2477@gmail.com>
2025-03-06 03:09:45 +0100

    feat: enable rls

---
+++ supabase/migrations/20250306014402_enable_rls.sql
@@ -0,0 +1,25 @@
+alter table "public"."beatmaps" enable row level security;
+
+alter table "public"."commentators" enable row level security;
+
+alter table "public"."mappool_maps" enable row level security;
+
+alter table "public"."mappools" enable row level security;
+
+alter table "public"."matches" enable row level security;
+
+alter table "public"."referees" enable row level security;
+
+alter table "public"."registrations" enable row level security;
+
+alter table "public"."scores" enable row level security;
+
+alter table "public"."streamers" enable row level security;
+
+alter table "public"."team_players" enable row level security;
+
+alter table "public"."teams" enable row level security;
+
+alter table "public"."tournaments" enable row level security;
+
+alter table "public"."users" enable row level security;
