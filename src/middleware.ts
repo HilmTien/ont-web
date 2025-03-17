d099aef7d372af94ae510606ea2003972bcf89e5
HilmTien <tient2477@gmail.com>
2025-03-17 03:52:52 +0100

    feat: add middleware for restricting access to admin pages

---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import { auth } from "@/lib/auth";
+import { NextResponse } from "next/server";
+import { createServerClient } from "./lib/server";
+
+export default auth(async (req) => {
+  if (!req.auth) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  if (req.auth.userId == parseInt(process.env.SITE_OWNER_OSU_ID!)) {
+    return;
+  }
+
+  const supabase = await createServerClient();
+
+  const { data: userWithAdmin } = await supabase
+    .from("users")
+    .select("id, admins(id)")
+    .eq("osu_id", req.auth.userId)
+    .single();
+
+  if (!userWithAdmin || userWithAdmin.admins.length === 0) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return;
+});
+
+export const config = {
+  matcher: "/admin/:path*",
+};
