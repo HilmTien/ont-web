a659d729a6e32847c7cf55abd0a8b8c1d05ae8e5
TonyWorep <tonyworep@gmail.com>
2025-06-11 00:05:17 +0200

    fix: supabase update filter

--- src/actions/beatmaps.ts
+++ src/actions/beatmaps.ts
@@ -33,23 +33,26 @@ export async function addBeatmap(id: number): Promise<ServerActionResponse> {
       cover: submittedBeatmap.beatmapset.covers.cover,
     });
   } else if (submittedBeatmap.last_updated > beatmap.last_updated) {
-    await supabase.from("beatmaps").update({
-      name: submittedBeatmap.beatmapset.title,
-      artist: submittedBeatmap.beatmapset.artist,
-      difficulty_name: submittedBeatmap.version,
-      mapper: submittedBeatmap.owners[0].username,
-      drain_time: submittedBeatmap.hit_length,
-      star_rating: submittedBeatmap.difficulty_rating,
-      bpm: submittedBeatmap.bpm,
-      mapset_host: submittedBeatmap.beatmapset.creator,
-      last_updated: submittedBeatmap.last_updated,
-      cs: submittedBeatmap.cs,
-      ar: submittedBeatmap.ar,
-      od: submittedBeatmap.accuracy,
-      hp: submittedBeatmap.drain,
-      osu_id: id,
-      cover: submittedBeatmap.beatmapset.covers.cover,
-    });
+    await supabase
+      .from("beatmaps")
+      .update({
+        name: submittedBeatmap.beatmapset.title,
+        artist: submittedBeatmap.beatmapset.artist,
+        difficulty_name: submittedBeatmap.version,
+        mapper: submittedBeatmap.owners[0].username,
+        drain_time: submittedBeatmap.hit_length,
+        star_rating: submittedBeatmap.difficulty_rating,
+        bpm: submittedBeatmap.bpm,
+        mapset_host: submittedBeatmap.beatmapset.creator,
+        last_updated: submittedBeatmap.last_updated,
+        cs: submittedBeatmap.cs,
+        ar: submittedBeatmap.ar,
+        od: submittedBeatmap.accuracy,
+        hp: submittedBeatmap.drain,
+        osu_id: id,
+        cover: submittedBeatmap.beatmapset.covers.cover,
+      })
+      .eq("osu_id", id);
   } else {
     console.log("The beatmap is already in the database.");
   }
