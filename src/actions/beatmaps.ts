512e8946d1e86ddc411c5dbcecb30a17f8c4acd7
TonyWorep <tonyworep@gmail.com>
2025-06-10 23:59:36 +0200

    fix: moved findbeatmap function to utils

--- src/actions/beatmaps.ts
+++ src/actions/beatmaps.ts
@@ -1,42 +1,8 @@
 "use server";
 
-import { auth } from "@/auth";
+import { findBeatmap } from "@/lib/beatmaps/utils";
 import { ServerActionResponse } from "@/lib/error";
 import { createServerClient } from "@/lib/server";
-
-export async function findBeatmap(id: number) {
-  const session = await auth();
-
-  const response = await fetch(`https://osu.ppy.sh/api/v2/beatmaps/${id}`, {
-    headers: {
-      Authorization: `Bearer ${session?.accessToken}`,
-    },
-  });
-
-  if (!response.ok) {
-    throw new Error(await response.text());
-  }
-  const beatmap = (await response.json()) as {
-    beatmapset: {
-      title: string;
-      artist: string;
-      covers: { cover: string };
-      creator: string;
-    };
-    version: string;
-    owners: [{ username: string }];
-    hit_length: number;
-    difficulty_rating: number;
-    bpm: number;
-    last_updated: string;
-    cs: number;
-    ar: number;
-    accuracy: number;
-    drain: number;
-  };
-
-  return beatmap;
-}
 
 export async function addBeatmap(id: number): Promise<ServerActionResponse> {
   const supabase = await createServerClient();
