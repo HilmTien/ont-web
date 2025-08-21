e89cfb4c08721772e2c8bc134f5d55edb4b82102
HilmTien <tient2477@gmail.com>
2025-08-21 21:26:06 +0200

    fix: supazod

--- src/actions/schedule.ts
+++ src/actions/schedule.ts
@@ -1,7 +1,7 @@
-import { PublicMatchesInsertSchema } from "@/generated/zod-schema-types";
+import { PublicMatchesInsert } from "@/generated/zod-schema-types";
 import { createServerClient } from "@/lib/server";
 
-export async function createMatch(match: PublicMatchesInsertSchema) {
+export async function createMatch(match: PublicMatchesInsert) {
   const supabase = await createServerClient();
 
   supabase.from("matches").insert(match);
