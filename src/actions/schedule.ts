0350e6518c7c430daba1d197b233442441d77f27
HilmTien <tient2477@gmail.com>
2025-08-27 20:47:53 +0200

    feat: add revalidation for adding matches

--- src/actions/schedule.ts
+++ src/actions/schedule.ts
@@ -5,11 +5,14 @@ import {
   PublicMatchesUpdate,
 } from "@/generated/zod-schema-types";
 import { createServerClient } from "@/lib/server";
+import { revalidatePath } from "next/cache";
 
 export async function createMatch(match: PublicMatchesInsert) {
   const supabase = await createServerClient();
 
   await supabase.from("matches").insert(match);
+
+  revalidatePath(`/admin/tournaments/${match.tournament_id}/schedule`);
 }
 
 export async function editMatch(match: PublicMatchesUpdate, id: number) {
