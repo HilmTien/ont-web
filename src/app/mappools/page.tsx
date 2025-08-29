59043c338d580174df8d561a5eced9b07c279237
TonyWorep <tonyworep@gmail.com>
2025-08-29 17:22:28 +0200

    feat: redirects on mappools and stats

--- src/app/mappools/page.tsx
+++ src/app/mappools/page.tsx
@@ -1,5 +1,19 @@
-import { MappoolsStageSelector } from "@/components/mappools/mappools-stage-selector";
+import { createServerClient } from "@/lib/server";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
-  return <MappoolsStageSelector />;
+  const supabase = await createServerClient();
+
+  const { data: stages } = await supabase
+    .from("tournament_stages")
+    .select("stage_index, is_public")
+    .eq("tournament_id", 1)
+    .eq("is_public", true)
+    .order("stage_index");
+
+  if (!stages) {
+    return <>Error no stages</>;
+  }
+
+  return redirect(`/mappools/${stages[stages.length - 1].stage_index}`);
 }
--- src/app/statistics/page.tsx
+++ src/app/statistics/page.tsx
@@ -1,5 +1,19 @@
-import { StatisticsStageSelector } from "@/components/statistics/statistics-stage-selector";
+import { createServerClient } from "@/lib/server";
+import { redirect } from "next/navigation";
 
-export default function Page() {
-  return <StatisticsStageSelector />;
+export default async function Page() {
+  const supabase = await createServerClient();
+
+  const { data: stages } = await supabase
+    .from("tournament_stages")
+    .select("stage_index, is_public")
+    .eq("tournament_id", 1)
+    .eq("is_public", true)
+    .order("stage_index");
+
+  if (!stages) {
+    return <>Error no stages</>;
+  }
+
+  return redirect(`/statistics/${stages[stages.length - 1].stage_index}`);
 }
