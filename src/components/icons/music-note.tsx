03bd53295c780a8b19f09ca1d63b0beaf8b63748
HilmTien <tient2477@gmail.com>
2025-08-30 21:56:59 +0200

    refactor: refactor icons to be more customizable

--- src/components/icons/music-note.tsx
+++ src/components/icons/music-note.tsx
@@ -1,12 +1,12 @@
-export default function MusicNote() {
+export default function MusicNote({ className }: IconProps) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
       stroke-width="1.5"
-      stroke="#d91c5d"
-      className="size-5"
+      stroke="currentColor"
+      className={`size-5 ${className}`}
     >
       <path
         stroke-linecap="round"
