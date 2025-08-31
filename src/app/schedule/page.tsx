5d9a462f79a8e620158c1e53deb47ec094d33569
TonyWorep <tonyworep@gmail.com>
2025-08-31 21:13:05 +0200

    fix: norsk

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -15,7 +15,7 @@ export default async function Page() {
   if (!stages || stages.length == 0) {
     return (
       <Content>
-        <p className="mx-auto my-2">No stages</p>
+        <p className="mx-auto my-2">Ingen stages</p>
       </Content>
     );
   }
