da36c44b546cfc3d5166eab5f0c36e33b24a8dd6
HilmTien <tient2477@gmail.com>
2025-09-01 19:51:35 +0200

    feat: add rest of info

--- src/components/info/info-view.tsx
+++ src/components/info/info-view.tsx
@@ -1,4 +1,6 @@
+import { BehaviourInfo } from "./sections/behaviour-info";
 import { GeneralInfo } from "./sections/general-info";
+import { MappoolInfo } from "./sections/mappool-info";
 import { MatchInfo } from "./sections/match-info";
 
 export function InfoView() {
@@ -6,6 +8,11 @@ export function InfoView() {
     <div className="flex flex-col gap-4 p-4">
       <GeneralInfo />
       <MatchInfo />
+      <MappoolInfo />
+      <BehaviourInfo />
+      <p className="font-bold">
+        Husk at ved usikkerhet kan man alltid spørre i Discorden!
+      </p>
     </div>
   );
 }
---
+++ src/components/info/sections/behaviour-info.tsx
@@ -0,0 +1,83 @@
+import { InfoSection } from "@/components/general/info-section";
+
+export function BehaviourInfo() {
+  return (
+    <InfoSection heading="Overordnede regler">
+      <ul className="list-disc">
+        <li>
+          Alle spillerene må ha{" "}
+          <strong>
+            <span className="text-accent">god og sportslig oppførsel</span>
+          </strong>
+          . All tegn på{" "}
+          <strong>
+            <span className="text-accent">
+              hat, trakassering og verbalt misbruk
+            </span>
+          </strong>{" "}
+          vil føre til{" "}
+          <strong>
+            <span className="text-accent">diskvalifikasjon</span>
+          </strong>{" "}
+          for{" "}
+          <strong>
+            <span className="text-accent">spilleren</span>
+          </strong>{" "}
+          og potensielt en{" "}
+          <strong>
+            <span className="text-accent">
+              turneringsban fra alle offisielle turneringer selv utenom o!NT
+            </span>
+          </strong>
+          !
+        </li>
+        <li>
+          Dette inkluderer{" "}
+          <strong>
+            <span className="text-accent">hatefulle ytringer</span>
+          </strong>
+          , bruk av{" "}
+          <strong>
+            <span className="text-accent">skjellsord</span>
+          </strong>
+          , og annen{" "}
+          <strong>
+            <span className="text-accent">krenkende symbolikk</span>
+          </strong>
+          . For eksempel er "tegning" med smoke av swastikasymbolet et{" "}
+          <strong>
+            <span className="text-accent">grovt brudd</span>
+          </strong>{" "}
+          på denne regelen, spesielt hvis matchen blir strømmet.{" "}
+          <strong>
+            <span className="text-accent">Alle involvert</span>
+          </strong>{" "}
+          i slik oppførsel vil bli{" "}
+          <strong>
+            <span className="text-accent">straffet</span>
+          </strong>
+          .
+        </li>
+        <li>
+          Dersom man omberammer kampen sin{" "}
+          <strong>
+            <span className="text-accent">mister man strømmeretten</span>
+          </strong>{" "}
+          i favør mot matchen som allerede er i tidsrammen.
+        </li>
+        <li>
+          En match med endret tid er den nye tiden. Det går ikke an å reversere
+          en omberamming dersom begge partene ikke er i enighet.
+        </li>
+        <li>
+          Dersom spillerene ikke kommer i enighet om en tid, vil matchen bli
+          spilt på den{" "}
+          <strong>
+            <span className="text-accent">opprinnelige tiden</span>
+          </strong>
+          .
+        </li>
+      </ul>
+    </InfoSection>
+  );
+}
