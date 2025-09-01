4ecd9ddacaf45032556cc15181be06b610caf311
HilmTien <tient2477@gmail.com>
2025-09-01 19:54:38 +0200

    feat: add required report button

--- src/components/info/info-view.tsx
+++ src/components/info/info-view.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { BehaviourInfo } from "./sections/behaviour-info";
 import { GeneralInfo } from "./sections/general-info";
 import { MappoolInfo } from "./sections/mappool-info";
@@ -13,6 +14,13 @@ export function InfoView() {
       <p className="font-bold">
         Husk at ved usikkerhet kan man alltid spørre i Discorden!
       </p>
+      <Link
+        href={"https://tcomm.hivie.tn/reports/create"}
+        target="_blank"
+        className="bg-accent flex w-full items-center justify-center p-3 text-xl"
+      >
+        Klikk her for å rapportere turneringer
+      </Link>
     </div>
   );
 }
