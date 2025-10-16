83c132ec5efcfe9b2873143e7e017da562a9f943
HilmTien <tient2477@gmail.com>
2025-10-17 01:36:33 +0200

    fix: production merge error

--- src/components/landing-page/landing-page-app.tsx
+++ src/components/landing-page/landing-page-app.tsx
@@ -1,7 +1,6 @@
 import { signIn } from "@/auth";
 import { getLandingPageMatches } from "@/lib/landing-page/query";
 import { createServerClient } from "@/lib/server";
-import { formatSecondsToTime } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import Discord from "../icons/discord";
